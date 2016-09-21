bd08a44fd3992d9209dc5d91468efe4edfae9d6a
Jeff Walter <jeff@404ster.com>
2016-09-21 00:44:34 -0700

    Odd fix

--- zsh-plugin-env.plugin.zsh
+++ zsh-plugin-env.plugin.zsh
@@ -5,9 +5,12 @@ reloadenv() {
 	if [ -n "${HOME}" ] && [ -d "${HOME}/env" ]; then
 		#shellcheck disable=2162,2164
 		while read NAME; do
-			#shellcheck disable=2086
-			eval export ${NAME}="$(<"${HOME}/env/${NAME}")"
-		done <<<"$(cd "${HOME}/env"; ls -1)"
+			NAME="$(basename "${NAME}")"
+			if [ -n "${NAME}" ]; then
+				#shellcheck disable=2046,2086
+				eval export ${NAME}=\"$(<"${HOME}/env/${NAME}")\"
+			fi
+		done <<<"$(find "${HOME}/env" -mindepth 1 -maxdepth 1 -type f)"
 	fi
 }
 
